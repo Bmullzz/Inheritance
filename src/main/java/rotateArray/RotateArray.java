b60a7aa41fd774c814b73b4772355823cae93d0e
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-02 17:40:36 -0400

    RotateArray almost done

---
+++ src/main/java/rotateArray/RotateArray.java
@@ -0,0 +1,63 @@
+package rotateArray;
+
+
+import java.util.*;
+
+/**
+ * Created by brianmullin on 5/1/17.
+ */
+public class RotateArray
+{
+
+    public static void main(String[] args)
+    {
+
+        public class numberArray
+        {
+
+/*
+        ArrayList<Num> num = new Arraylist<1, 2, 3, 4, 5>;  //Create new array list
+
+        int rotateBy = 3;
+
+        //add elements to array list
+        num.add(1);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        num.add(5);
+        num.add(6);
+
+        for(ArrayList<> = 0; rotateBy < 6; rotateBy++) {
+
+
+
+
+        }
+
+        System.out.print(num);
+        */
+
+            int numberArray[] = {1, 2, 3, 4, 5, 6};
+
+
+            int rotateBy = 2;
+
+            for(int i = 0;i<rotateBy;i++)
+            {
+
+                for (int j = numberArray.length - 1; j > 0; j--)
+                {
+
+                    int temp = numberArray[j];
+
+                    numberArray[j] = numberArray[j - 1];
+
+                    System.out.print(numberArray);
+
+
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/superHumans/superHumans.java
@@ -0,0 +1,9 @@
+package superHumans;
+
+/**
+ * Created by brianmullin on 5/2/17.
+ */
+public class superHumans {
+
+    public class s
+}
