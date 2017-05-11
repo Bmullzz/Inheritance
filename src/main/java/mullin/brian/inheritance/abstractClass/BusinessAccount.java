dfd8d01d86383480b27ddc92d71ff7c30265bfbd
Brian Mullin <brmullin@email.msmary.edu>
2017-05-11 18:38:52 -0400

    Polymorphism for the win exclamation pointssss amended author email

--- src/main/java/mullin/brian/inheritance/abstractClass/BusinessAccount.java
+++ src/main/java/mullin/brian/inheritance/abstractClass/BusinessAccount.java
@@ -4,7 +4,11 @@ package mullin.brian.inheritance.abstractClass;
  * Created by brianmullin on 5/4/17.
  */
 
-/*
+
 public class BusinessAccount extends Account {
 
-}*/
+
+    public BusinessAccount(int accountNum, double balance) {
+        super(accountNum, balance);
+    }
+}
--- src/main/java/mullin/brian/inheritance/abstractClass/CheckingAccount.java
+++ src/main/java/mullin/brian/inheritance/abstractClass/CheckingAccount.java
@@ -4,6 +4,10 @@ package mullin.brian.inheritance.abstractClass;
  * Created by brianmullin on 5/4/17.
  */
 
-/*
+
 public class CheckingAccount extends Account {
-}*/
+
+    public CheckingAccount(int accountNum, double balance) {
+        super(accountNum, balance);
+    }
+}
